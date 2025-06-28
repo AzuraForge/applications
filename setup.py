40dad0281d0ebb1a7219015bc54f1b72154a771a
Azmi ŞAHİN <6445951+azmisahin@users.noreply.github.com>
2025-06-28 04:36:46 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# ========== DOSYA: applications/setup.py ==========
+from setuptools import setup, find_packages
+
+setup(
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    # EN ÖNEMLİ KISIM: Paket kurulduğunda .json dosyasının da kopyalanmasını sağlar
+    include_package_data=True, 
+    package_data={
+        "azuraforge_apps_catalog": ["*.json"],
+    },
+)
